4d405ed1d2749652459a3b0f19630197c6554b53
Ioannis Kokkinis <info@upg.gr>
2017-10-22 15:53:39 +0300

    Updated ielko-video-player.js.

--- js/ielko-video-player.js
+++ js/ielko-video-player.js
@@ -1,6 +1,20 @@
 function playmedia(type, url) {
   switch (type) {
     case 'm3u8':
+    console.log(
+      '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background"> \
+      <video id="upgvideoplayer" \
+      class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" \
+      controls \
+      autoplay \
+      data-setup="{ \
+      "sources": [{ \
+      "type:"application/x-mpegurl", \
+      "src:"' + url + '" \
+      }]}"  > \
+      </video></div> \
+      '
+    )
       $(".upgvid").empty().append(
         '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background"> \
         <video id="upgvideoplayer" \
