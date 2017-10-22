7307e403bfac35da75c4c3feaa884328536456f0
Ioannis Kokkinis <info@upg.gr>
2017-10-22 15:28:04 +0300

    Updated ielko-video-player.jsplay.html .

--- js/ielko-video-player.js
+++ js/ielko-video-player.js
@@ -1,25 +1,24 @@
-function playmedia(type,url) {
-switch (type) {
-case 'm3u8':
-  $(".upgvid").empty().append(
-    '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background"><video id="upgvideoplayer" poster="" class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" controls preload="auto" autoplay data-setup={"fluid":true,"plugins":{"airplayButton":{}}} playsinline ><source type="application/x-mpegurl" src="' +
-    play + '"></video></div>');
-break;
+function playmedia(type, url) {
+  switch (type) {
+    case 'm3u8':
+      $(".upgvid").empty().append(
+        '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background"><video id="upgvideoplayer" poster="" class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" controls preload="auto" autoplay data-setup={"fluid":true,"plugins":{"airplayButton":{}}} playsinline ><source type="application/x-mpegurl" src="' +
+        play + '"></video></div>');
+      break;
 
-case 'youtube':
-    $(".upgvid").empty().append(
-      '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background">' +
-      '<video' +
-      'id="upgvideoplayer"' +
-      'class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" controls autoplay ' +
-      'data-setup="{' +
-      '"techOrder": ["youtube"],' +
-      '"sources": [{' +
-      '"type": "video/youtube",' +
-      '"src": "https://www.youtube.com/watch?v=xjS6SftYQaQ"' +
-      '}]}>' +
-      '</video></div>'
-    );
-    break;
-    
+    case 'youtube':
+      $(".upgvid").empty().append(
+        '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background">' +
+        '<video' +
+        'id="upgvideoplayer"' +
+        'class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" controls autoplay ' +
+        'data-setup="{' +
+        '"techOrder": ["youtube"],' +
+        '"sources": [{' +
+        '"type": "video/youtube",' +
+        '"src": "https://www.youtube.com/watch?v=xjS6SftYQaQ"' +
+        '}]}>' +
+        '</video></div>');
+      break;
 }
+  }
