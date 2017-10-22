18d3e58c1be8a4ad94f1570e233f33739b20c473
Ioannis Kokkinis <info@upg.gr>
2017-10-22 16:03:01 +0300

    Updated ielko-video-player.js.

--- js/ielko-video-player.js
+++ js/ielko-video-player.js
@@ -1,34 +1,27 @@
 function playmedia(type, url) {
   switch (type) {
     case 'm3u8':
-      $(".upgvid").empty().append(
-        '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background"> \
-        <video id="upgvideoplayer" \
-        class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" \
-        controls \
-        autoplay \
-        data-setup=\'{ \
-        "sources": [{ \
-        "type":"application/x-mpegurl", \
-        "src":"' + url + '" \
-      }]}\' > \
-        </video></div> \
-        ');
+      thetype = 'application/x-mpegurl';
+      techordernode = '';
       break;
-
     case 'youtube':
-      $(".upgvid").empty().append(
-        '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background">' +
-        '<video' +
-        'id="upgvideoplayer"' +
-        'class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" controls autoplay ' +
-        'data-setup="{' +
-        '"techOrder": ["youtube"],' +
-        '"sources": [{' +
-        '"type": "video/youtube",' +
-        '"src": "https://www.youtube.com/watch?v=xjS6SftYQaQ"' +
-        '}]}>' +
-        '</video></div>');
+      thetype = 'video/youtube';
+      techordernode = '"techOrder": ["youtube"]';
       break;
   }
+
+  $(".upgvid").empty().append(
+    '<div id="videoHolder" style="background-image:url();" data-embed="true" class="flowplayer-video videoHolder-322 videoHolder fullscreen-bg playful no-background"> \
+    <video id="upgvideoplayer" \
+    class="video-js vjs-default-skin vjs-fluid vjs-16-9 vjs-big-play-centered fullscreen-bg__video" \
+    controls \
+    autoplay \
+    data-setup=\'{ \
+    "sources": [{ \
+'+techordernode+' \
+    "type":"' + thetype + '", \
+    "src":"' + url + '" \
+  }]}\' > \
+    </video></div> \
+    ');
 }
